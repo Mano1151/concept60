530c85a2c20da4e6791b515e060580794ffc01a9
Mano1151 <manom1151.sse@saveetha.com>
2026-06-14 02:16:03 +0530

    React mount test

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <div style={{ color: 'white', padding: '30px' }}>
-    React Mounted Successfully
-  </div>
+  <App />
 );
