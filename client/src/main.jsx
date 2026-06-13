07affebd407c1e48f2aeb2e520e792f64eec4464
Mano1151 <manom1151.sse@saveetha.com>
2026-06-14 02:19:01 +0530

    React mount test

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import { AuthProvider } from './context/AuthContext';
+import { ToastProvider } from './context/ToastContext';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
+  <React.StrictMode>
+    <AuthProvider>
+      <ToastProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ToastProvider>
+    </AuthProvider>
+  </React.StrictMode>
 );
